5921f1f522922222adf757a84c8170c24fcee700
Softwarica College Lab <softwarica@softwarica.com>
2019-04-28 11:28:05 +0545

    loginvalidationhalf

--- app/src/main/java/com/example/tourguide/LoginActivity.java
+++ app/src/main/java/com/example/tourguide/LoginActivity.java
@@ -1,13 +1,38 @@
 package com.example.tourguide;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText etUserName,etPassword;
+    private Button btnLOgIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        etUserName=findViewById(R.id.etUserName);
+        etPassword=findViewById(R.id.etPassword);
+        btnLOgIn=findViewById(R.id.btnLOgIn);
+
+        btnLOgIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+    private void checkUser(){
+        SharedPreferences sharedPreferences=getSharedPreferences("User", MODE_PRIVATE);
+        String username= sharedPreferences.getString("username", "");
+        String password= sharedPreferences.getString("password", "");
+        if (username.equals(etUserName.getText().toString())){
+            
+        }
+
     }
 }
